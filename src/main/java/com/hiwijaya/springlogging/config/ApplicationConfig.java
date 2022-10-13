d7956dfc8c86588524c8e13ba03394e24215d680
Happy Indra Wijaya <dev.hiwijaya@gmail.com>
2022-10-13 18:40:30 +0700

    add built-in RequestLoggingFilter spring

---
+++ src/main/java/com/hiwijaya/springlogging/config/ApplicationConfig.java
@@ -0,0 +1,22 @@
+package com.hiwijaya.springlogging.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+@Configuration
+public class ApplicationConfig {
+
+    @Bean
+    public CommonsRequestLoggingFilter logFilter() {
+        CommonsRequestLoggingFilter filter = new CommonsRequestLoggingFilter();
+        filter.setIncludeQueryString(true);
+        filter.setIncludePayload(true);
+        filter.setMaxPayloadLength(10000);
+        filter.setIncludeHeaders(false);
+        filter.setAfterMessagePrefix("REQUEST DATA: ");
+
+        return filter;
+    }
+
+}
